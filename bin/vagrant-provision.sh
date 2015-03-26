34cbbebb85217e5a1cedf541277aceb467ae5a10
David Otton <david@otton.org>
2015-03-26 18:44:02 +0000

    components needed to make php

--- bin/vagrant-provision.sh
+++ bin/vagrant-provision.sh
@@ -10,34 +10,41 @@ then
   to_provision='/vagrant/vagrant.pp'
 fi
 
+# runs once
+if [ ! -f /vagrant/keys/private_key.pkcs7.pem ];
+then
+    cd /home/vagrant
+    eyaml createkeys
+fi
+
 # Don't change config if there are already keys
 if [ ! -f /home/vagrant/.ssh/id_rsa ];
 then
+    yum install puppet -y
+    yum install ruby -y
+    yum install gcc g++ make automake autoconf curl-devel openssl-devel zlib-devel httpd-devel apr-devel apr-util-devel sqlite-devel -y
+    yum install ruby-rdoc ruby-devel -y
+    yum install rubygems -y
 
-#    yum install puppet -y
-#    yum install ruby -y
-#    yum install gcc g++ make automake autoconf curl-devel openssl-devel zlib-devel httpd-devel apr-devel apr-util-devel sqlite-devel -y
-#    yum install ruby-rdoc ruby-devel -y
-#    yum install rubygems -y
-#    gem update
-#    gem install hiera
-#    gem install hiera-eyaml
+    # for building php 5.2.x
+    yum install libxml2 libxml2-devel pcre-devel bzip2-devel libjpeg-devel libpng-devel libXpm-devel -y
+    yum install freetype-devel gmp-devel mysql-devel unixODBC-devel readline-devel net-snmp-devel -y
+    yum install libxslt-devel -y
 
+    gem update
+    gem install hiera
+    gem install hiera-eyaml
     gem install hiera-puppet
-    ln -s /usr/lib/ruby/gems/1.8/gems/hiera-puppet-1.0.0/ /vagrant/puppet/modules/hiera-puppet
 
+    rm -rf /vagrant/puppet/modules/hiera-puppet
+    ln -s /usr/lib/ruby/gems/1.8/gems/hiera-puppet-1.0.0/ /vagrant/puppet/modules/hiera-puppet
     rm -rf /etc/puppet
     ln -sf /vagrant/puppet /etc/
-    ln -sf /etc/puppet/hiera.yaml /etc/
+    ln -sf /vagrant/puppet/hiera.yaml /etc/
+    ln -sf /vagrant/hieradata /etc/
 
-    if [ ! -f /vagrant/keys/private_key.pkcs7.pem ];
-    then
-        cd /home/vagrant
-        eyaml createkeys
-    fi
 fi
 
-ln -sf /vagrant/hieradata /etc/
 
 /usr/bin/puppet apply --verbose \
   --manifestdir=${puppet_base}/manifests \
