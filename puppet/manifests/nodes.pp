aba12f578e4934948261243ad7ca352857564fe3
David Otton <david@otton.org>
2015-03-26 18:47:07 +0000

    build PHP 5.2.17 from source

--- puppet/manifests/nodes.pp
+++ puppet/manifests/nodes.pp
@@ -3,9 +3,35 @@ node default {
 }
 
 node wuk-dev {
-  class {'beluga::apache_frontend_server': }
-
   class {'beluga::mysql_server': }
+
+  class { 'apache':
+    mpm_module                  => 'prefork',
+  }
+  #include apache::mod::php
+  include apache::mod::rewrite
+
+  exec { 'download-php52':
+    cwd     => '/vagrant',
+    command => 'sudo curl -O http://museum.php.net/php5/php-5.2.17.tar.gz',
+    path    => '/usr/local/bin/:/usr/bin/:/bin/',
+  } ->
+  exec { "extract-php52":
+    cwd     => "/vagrant",
+    command => "tar -zxvf php-5.2.17.tar.gz",
+    path    => "/usr/local/bin/:/usr/bin/:/bin/",
+  } ->
+  exec { "make-php52":
+    cwd     => "/vagrant/php-5.2.17",
+    command => "bash ./configure --with-apxs2 --with-libdir=lib64 --build=x86_64-redhat-linux-gnu --host=x86_64-redhat-linux-gnu --target=x86_64-redhat-linux-gnu --program-prefix= --prefix=/usr --exec-prefix=/usr --bindir=/usr/bin --sbindir=/usr/sbin --sysconfdir=/etc --datadir=/usr/share --includedir=/usr/include --libdir=/usr/lib64 --libexecdir=/usr/libexec --localstatedir=/var --sharedstatedir=/usr/com --mandir=/usr/share/man --infodir=/usr/share/info --cache-file=../config.cache --with-libdir=lib64 --with-config-file-path=/etc --with-config-file-scan-dir=/etc/php.d --disable-debug --with-pic --disable-rpath --without-pear --with-bz2 --with-curl --with-exec-dir=/usr/bin --with-freetype-dir=/usr --with-png-dir=/usr --enable-gd-native-ttf --without-gdbm --with-gettext --with-gmp --with-iconv --with-jpeg-dir=/usr --with-openssl --with-pcre-regex=/usr --with-zlib --with-layout=GNU --enable-exif --enable-ftp --enable-magic-quotes --enable-sockets --enable-sysvsem --enable-sysvshm --enable-sysvmsg --enable-wddx --with-kerberos --enable-ucd-snmp-hack --with-unixODBC=shared,/usr --enable-shmop --enable-calendar --without-mime-magic --without-sqlite --with-libxml-dir=/usr --enable-force-cgi-redirect --enable-pcntl --enable-mbstring=shared --enable-mbregex --with-gd=shared --enable-bcmath=shared --enable-dba=shared --with-db4=/usr --with-xmlrpc=shared --with-ldap=shared --with-ldap-sasl --with-mysql --with-mysqli=shared,/usr/bin/mysql_config --enable-dom=shared --with-snmp=shared,/usr --enable-soap=shared --with-xsl=shared,/usr --enable-xmlreader=shared --enable-xmlwriter=shared --enable-fastcgi --enable-pdo --with-pdo-odbc=shared,unixODBC,/usr --with-pdo-mysql --enable-json=shared --enable-zip=shared --with-readline --with-xpm-dir=/usr",
+    path    => "/usr/local/bin/:/usr/bin/:/bin/",
+  } ->
+  exec { "install-php52":
+    cwd     => "/vagrant/php-5.2.17",
+    command => "make -j 2 && sudo make install",
+    path    => "/usr/local/bin/:/usr/bin/:/bin/",
+  }
+
 }
 
 node /.*\.dgudev/ {
@@ -18,10 +44,6 @@ node /.*\.dgudev/ {
   include beluga::drush_server
   include beluga::mysql_server
   include beluga::ruby_frontend
-  class { 'ckan':
-    nginx_vhost => 'dgud7',
-    nginx_port  => 8880,
-  }
   include dgu_defaults
   include memcached
   include orgdc
