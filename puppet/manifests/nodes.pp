f89646e541748e9e1a557762542ce214439772d8
David Otton <david@otton.org>
2015-03-27 19:41:57 +0000

    New vhost

--- puppet/manifests/nodes.pp
+++ puppet/manifests/nodes.pp
@@ -10,6 +10,7 @@ node wuk-dev {
   }
   include apache::mod::rewrite
   include apache::mod::passenger
+  include apache::mod::php
 
   file { [ "/sites", "/sites/intranet", "/sites/intranet2" ]:
     ensure                      => "directory",
@@ -156,9 +157,9 @@ SetEnvIfNoCase Request_URI random_supplier_image\.(?:php)$ no-gzip dont-vary',
         path                    => '/sites/intranet2/wuk',
         options                 => ['FollowSymLinks', '-MultiViews'],
         php_values              => ['include_path "/sites/intranet2/wuk"', 'include_path "/sites/intranet2/pip"'],
-        php_flag                => 'zend.ze1_compatibility_mode Off',
         custom_fragment         => 'RailsBaseURI /apps
 RailsBaseURI /qna',
+# zend.ze1_compatibility_mode Off',
       },
       {
         allow_override          => ['All'],
@@ -166,6 +167,19 @@ RailsBaseURI /qna',
         path                    => '/sites/intranet2/pip',
       },
     ],
+  }
+
+  apache::vhost { 'dev.groupintranet.wolseley.co.uk':
+    docroot                     => '/sites/intranet/group',
+    directories                 => [
+      {
+        path                    => '/sites/intranet/group',
+        options                 => ['SymLinksIfOwnerMatch'],
+        allow_override          => ['None'],
+        php_values              => ['include_path /sites/intranet/group', 'session.save_path /tmp'],
+        custom_fragment         => 'php_flag magic_quotes_gpc On',
+      }
+    ]
   }
 
   exec { 'download-php52':
